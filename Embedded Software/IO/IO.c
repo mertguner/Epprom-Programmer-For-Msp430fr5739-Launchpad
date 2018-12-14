8eed967015fe7a5e652f2a0132566de907d29c9d
Mert Güner <mert.guner1@ogr.sakarya.edu.tr>
2018-12-15 01:00:28 +0300

    Add files via upload

--- Embedded Software/IO/IO.c
+++ Embedded Software/IO/IO.c
@@ -1,1 +1,15 @@
+#include <msp430.h>
+#include "IO.h"
+#include "../headers/GenelTanimlar.h"
+
+void IO_Init()
+{
+	PinDefault();
+
+	//-------------------------------- Ledler 3 Adet --------------------------------//
+	PinLow(P1OUT,BIT0 | BIT1);
+    PinOutput(P1DIR, BIT0 | BIT1);
+    /*-------------------------------- Ledler 3 Adet --------------------------------*/
+
+}
 
